1c9cb68bfb7ad6bb37ffbb1297179ee5d957f5d1
Konstantinos Vaggelakos <kozze89@gmail.com>
2011-11-29 19:54:14 +0100

    Renamed the generated view to MainView

--- II2300-PRP/II2300-PRP/MainViewController.m
+++ II2300-PRP/II2300-PRP/MainViewController.m
@@ -6,9 +6,9 @@
 //  Copyright (c) 2011 fsefsef. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "MainViewController.h"
 
-@implementation ViewController
+@implementation MainViewController
 
 - (void)didReceiveMemoryWarning
 {
